73f9743ff2c21e8b1d3c49728dd6ac6e4705ac15
Mebin Mathew <mebinmathew006@gmail.com>
2025-02-18 10:57:20 +0530

    remove frontend for fresh start

--- FrontEnd/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore } from '@reduxjs/toolkit';
-import UserDetailsSlice from './UserDetailsSlice';
-const store = configureStore({
-    reducer:{
-        userDetails: UserDetailsSlice,
-    }
-})
-
-export default store;
