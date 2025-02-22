c79aa457f77f1cefc214b29a079163a6037788c9
Mebin Mathew <mebinmathew006@gmail.com>
2025-02-23 02:49:28 +0530

    modified asgi.py for ws

--- BackEnd/UrbanBalcony/asgi.py
+++ BackEnd/UrbanBalcony/asgi.py
@@ -7,11 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 # asgi.py
+# asgi.py
 import os
 import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UrbanBalcony.settings')
 django.setup()
@@ -20,7 +20,5 @@ from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
-        URLRouter(websocket_urlpatterns)
-    ),
+    "websocket": URLRouter(websocket_urlpatterns),  # Remove AllowedHostsOriginValidator
 })
